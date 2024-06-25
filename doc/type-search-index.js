4e374cee5a5d9f0707d56f220f685d2ea78727d8
Yacine Baghli <167008935+yacine-baghli@users.noreply.github.com>
2024-06-25 18:20:03 +0200

    Update

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"client","l":"ListeningThread"},{"p":"client","l":"MainClient"},{"p":"server","l":"MainServer"},{"p":"server","l":"Partie"},{"p":"tictactoe","l":"Plateau"}];updateSearchResults();
